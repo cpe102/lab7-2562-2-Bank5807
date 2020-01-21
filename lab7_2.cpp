b8840015e720d481cf592fa85b4066d1dbe814ec
Bank5807 <tanat_wip@cmu.ac.th>
2020-01-21 15:41:34 +0700

    lab7 v1

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -3,7 +3,19 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double x){
+    if(x > 90){
+        return 'A';
+    }else if(x > 75){
+        return 'B';
+    }else if(x > 60){
+        return 'C';
+    }else if(x > 45){
+        return 'D';
+    }else{
+        return 'F';
+    }
+}
 
 int main(){
 	//Input the number of students
@@ -11,18 +23,18 @@ int main(){
 	cout << "Enter the number of students: ";
 	cin >> N;
 	string name[N];
-	float score[N];	
-	
-	//Store names and scores of students into an array 
+	float score[N];
+
+	//Store names and scores of students into an array
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin, name[i]);
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
-	
+
 	//Print names scores and grades
 	i = 0;
 	cout << "---------------------------------------------\n";
@@ -31,8 +43,9 @@ int main(){
 	while(i < N){
 		cout << setw(25) << name[i] << setw(8) << score[i] << setw(8) << findGrade(score[i]) << "\n";
 		i++;
-	} 
+	}
 	cout << "---------------------------------------------\n";
 
 	return 0;
 }
+
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,41 @@
-//status = "ONE-NIGHT-STAND";
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age, height, asset;
+    string status;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age < 20){
+        cout << "Enter your height(centimeter): ";
+        cin >> height;
+        if(height < 160){
+            status = "UNFRIEND";
+        }else if(height < 175){
+            status = "FRIEND";
+        }else{
+            cout << "Enter your asset(million baht): ";
+            cin >> asset;
+            if(asset > 69){
+                status = "MARRIED";
+            }else{
+                status = "ONE-NIGHT STAND";
+            }
+        }
+    }else if(age < 30){
+        cout << "Enter your asset(million baht): ";
+        cin >> asset;
+        if(asset > 10){
+            status = "BEST FRIEND";
+        }else{
+            status = "UNFRIEND";
+        }
+    }else{
+        status = "UNFRIEND";
+    }
+
+    cout << "your status is \"" << status << "\"";
+    return 0;
+}
+
